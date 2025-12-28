1236608834ff6ba4784992c136934524482dac97
ailtondasilva95 <ailton.domingos.dias.da.silva@gmail.com>
2025-12-28 17:41:24 +0100

    Implementação do layout de tabela

--- resources/views/livewire/user-table.blade.php
+++ resources/views/livewire/user-table.blade.php
@@ -1,46 +1,47 @@
-<div class="overflow-x-auto rounded-lg shadow-md">
-    <table class="min-w-full border-collapse table-auto text-sm text-gray-700 dark:text-gray-200">
-        <thead class="bg-gray-100 dark:bg-gray-900 uppercase text-gray-600 dark:text-gray-300 tracking-wider">
-            <tr>
-                <th class="px-4 py-3 text-left border-b border-gray-300 dark:border-gray-700">ID</th>
-                <th class="px-4 py-3 text-left border-b border-gray-300 dark:border-gray-700">Name</th>
-                <th class="px-4 py-3 text-left border-b border-gray-300 dark:border-gray-700">Email</th>
-                <th class="px-4 py-3 text-left border-b border-gray-300 dark:border-gray-700">Created At</th>
-                <th class="px-4 py-3 text-center border-b border-gray-300 dark:border-gray-700"></th>
+<div class="overflow-x-auto rounded-2xl shadow-2xs border border-gray-200 dark:border-gray-700">
+    <table class="w-full table-auto text-sm">
+        <thead class="border-b bg-gray-100 dark:bg-gray-800">
+            <tr class="text-left">
+                <th class="px-4 py-2 font-semibold">ID</th>
+                <th class="px-4 py-2 font-semibold">Nome</th>
+                <th class="px-4 py-2 font-semibold">Email</th>
+                <th class="px-4 py-2 font-semibold whitespace-nowrap">Criado em</th>
+                <th></th>
             </tr>
         </thead>
-
-        <tbody class="bg-white dark:bg-gray-800 divide-y divide-gray-200 dark:divide-gray-700">
+        <tbody>
             @forelse ($users as $user)
-                <tr class="hover:bg-gray-50 dark:hover:bg-gray-700 transition-colors duration-150">
-                    <td class="px-4 py-2 border-b border-gray-200 dark:border-gray-700">{{ $user->id }}</td>
-                    <td class="px-4 py-2 border-b border-gray-200 dark:border-gray-700">{{ $user->name }}</td>
-                    <td class="px-4 py-2 border-b border-gray-200 dark:border-gray-700">{{ $user->email }}</td>
-                    <td class="px-4 py-2 border-b border-gray-200 dark:border-gray-700">
-                        {{ $user->created_at->format('d/m/Y H:i:s') }}
-                    </td>
-                    <td class="px-4 py-2 border-b border-gray-200 dark:border-gray-700 text-center">
-                        <div class="relative inline-block text-left">
-                            <x-dropdown class="absolute right-0 mt-2 w-48 bg-white dark:bg-gray-800 shadow-lg rounded-md ring-1 ring-black ring-opacity-5">
-                                <x-dropdown.item label="View" icon="eye" wire:click="view({{ $user->id }})" />
-                                <x-dropdown.item label="Edit" icon="pencil" wire:click="edit({{ $user->id }})" />
-                                <x-dropdown.item label="Delete" icon="trash" wire:click="delete({{ $user->id }})" />
-                            </x-dropdown>
-                        </div>
+                <tr class="border-b hover:bg-gray-100 dark:hover:text-black">
+                    <td class="px-4 py-2">{{ $user->id }}</td>
+                    <td class="px-4 py-2">{{ $user->name }}</td>
+                    <td class="px-4 py-2">{{ $user->email }}</td>
+                    <td class="px-4 py-2">{{ $user->created_at->format('d/m/Y H:i:s') }}</td>
+                    <td class="px-4 py-2 text-center">
+                        <x-dropdown>
+                            <x-dropdown.item label="Ver" icon="eye" wire:click="view({{ $user }})" />
+
+                            <x-dropdown.item label="Editar" icon="pencil" wire:click="edit({{ $user }})" />
+
+                            <x-dropdown.item label="Apagar" icon="trash" wire:click="delete({{ $user }})" />
+                        </x-dropdown>
                     </td>
                 </tr>
             @empty
-                <tr class="hover:bg-gray-50 dark:hover:bg-gray-700 transition-colors duration-150">
-                    <td colspan="5" class="px-4 py-2 border-b border-gray-200 dark:border-gray-700 text-center font-semibold">
+                <tr>
+                    <td colspan="5" class="p-4 text-center font-medium">
                         Nenhum usuário encontrado.
                     </td>
                 </tr>
             @endforelse
         </tbody>
+        <tfoot>
+            @if ($users->hasPages())
+                <tr>
+                    <td colspan="5" class="p-2 bg-gray-100 dark:bg-gray-800">
+                        {{ $users->links() }}
+                    </td>
+                </tr>
+            @endif
+        </tfoot>
     </table>
-
-    <!-- Paginação -->
-    <div class="mt-1 mb-1 p-1">
-        {{ $users->links() }}
-    </div>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,8 @@
 
     <livewire:user-table />
 
-    <x-password label="Secret 🙈" value="I love WireUI ❤️" />
+    <x-input label="Nome" placeholder="Digite seu nome" value="I love WireUI" />
 
-    <x-input label="Nome" placeholder="Digite seu nome" />
-
-    <x-button primary label="Salvar" />
+    <x-button label="Salvar" />
 
 </x-layouts.app>
